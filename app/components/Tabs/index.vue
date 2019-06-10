7884727b5cf898486ec3a48f0bcf5c8584d7a269
Luis Fernando Salazar Buitrago <luisfer7192@hotmail.com>
2019-06-10 10:23:17 -0500

    initial commit

---
+++ app/components/Tabs/index.vue
@@ -0,0 +1,59 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!" android:flat="true"/>
+        <TabView android:tabBackgroundColor="#53ba82"
+                 android:tabTextColor="#c4ffdf"
+                 android:selectedTabTextColor="#ffffff"
+                 androidSelectedTabHighlightColor="#ffffff">
+            <TabViewItem title="Home">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" :text="msg" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Chat">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Chat" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Search">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Search" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Notifications">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Notifications" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="User">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="User" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Welcome to Home!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
